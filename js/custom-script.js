0b5044078e9c472b028188d2a6137e242e529111
MuraliJbk <murali@jbkinfotech.com>
2023-06-30 17:15:53 +0530

    fixed header

--- js/custom-script.js
+++ js/custom-script.js
@@ -4,4 +4,9 @@ let header = jQuery(".main-header");
     let scroll = jQuery(this).scrollTop();
     scroll >= 4 ? header.addClass("fixed-header") : header.removeClass("fixed-header");
   });
+  jQuery(".toggle_icon").on("click", function(e){
+    e.preventDefault();
+    jQuery(this).toggleClass("open");
+    jQuery(".main-header").toggleClass("nav-open");
+  });
 });
