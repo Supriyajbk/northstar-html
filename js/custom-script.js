5f4b2195d2c15bc4de3c91f4d893e4bee458dbbf
MuraliJbk <murali@jbkinfotech.com>
2023-06-30 21:17:30 +0530

    test

--- js/custom-script.js
+++ js/custom-script.js
@@ -8,5 +8,16 @@ let header = jQuery(".main-header");
     e.preventDefault();
     jQuery(this).toggleClass("open");
     jQuery(".main-header").toggleClass("nav-open");
+    jQuery(".navigation").toggleClass("open");
   });
+  if(jQuery(window).width() <= 1023){
+    jQuery("ul.main-menu > li.menu-item-has-children > a").on("click", function(event){
+      event.preventDefault();
+      jQuery('ul.main-menu > li.menu-item-has-children > a').not(jQuery(this)).removeClass('active');
+      jQuery(this).toggleClass("active");
+      jQuery(this).parent().siblings().find('ul.sub-menu').slideUp();
+      jQuery(this).next('ul.sub-menu').slideToggle();
+      jQuery(this).parent().siblings().toggleClass('sib');
+    });
+  }
 });
