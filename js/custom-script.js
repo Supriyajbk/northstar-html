39f2a3c539a22a51d11c28da23da6f3d1c2c30f5
MuraliJbk <murali@jbkinfotech.com>
2023-07-02 20:11:29 +0530

    test

--- js/custom-script.js
+++ js/custom-script.js
@@ -22,6 +22,12 @@ let header = jQuery(".main-header");
 
   jQuery(".financial-more-btn").on("click", function(e){
     e.preventDefault();
+    let list = jQuery(".financial-row");
+    let listItems = list.children(".financial-list");
+    if(listItems.length > 4){
+      jQuery(listItems).slice(4).fadeToggle(900);
+      jQuery(this).text(jQuery(this).text() == 'load more' ? 'no posts' : 'load more');
+    }
   });
 
 });
