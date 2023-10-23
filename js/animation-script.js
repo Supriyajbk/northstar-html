dca26ede9c62f5f4894c02e3ecc58dc093de838e
MuraliJbk <murali@jbkinfotech.com>
2023-10-23 15:05:56 +0530

    starts animation

--- js/animation-script.js
+++ js/animation-script.js
@@ -2,24 +2,18 @@
 let $animation_elements = jQuery('[data-animation]');
 let $window = jQuery(window);
 function check_if_in_view() {
-  let window_height = $window.height();
-  let window_top_position = $window.scrollTop();
-  let window_bottom_position = (window_top_position + window_height);
   $animation_elements.each(function() {
     let $element = jQuery(this);
     let $attr = $element.attr('data-animation');
-    let element_height = $element.outerHeight();
-    let element_top_position = $element.offset().top;
-    let element_bottom_position = (element_top_position + element_height);
+
     //check to see if this current container is within viewport
-    if ((element_bottom_position >= window_top_position) &&
-      (element_top_position <= window_bottom_position)) {
-      $element.addClass('visible');
+    if ($element.is(':in-viewport')) {
       $element.addClass($attr);
+      $element.addClass('visible');
     } else {
-      //$element.removeClass('visible');
+      $element.removeClass('visible');
     }
   });
 }
 $window.on('scroll load', check_if_in_view);
-$window.trigger('scroll load', check_if_in_view);
+$window.trigger('scroll load');
